e9903924542441fd0839a8e4f649f32f26d10d13
Filipp Lepalaan <filipp@mac.com>
2017-01-19 18:17:40 +0200

    Cleanup

--- servo/urls/checkin.py
+++ servo/urls/checkin.py
@@ -3,16 +3,16 @@
 from django.views.generic.base import RedirectView
 from django.conf.urls import url
 
-from servo.views.checkin import *
+from servo.views import checkin
 
 
 urlpatterns = [
-    url(r'^$', index, name='checkin-index'),
+    url(r'^$', checkin.index, name='checkin-index'),
     url(r'^sn/$', RedirectView.as_view(url='/checkin/', permanent=True)),
-    url(r'^customer/$', get_customer, name='checkin-get_customer'),
-    url(r'^reset/$', reset, name='checkin-reset'),
-    url(r'^status/$', status, name='checkin-status'),
-    url(r'^checkin/print/(\w+)/$', print_confirmation, name='checkin-print'),
-    url(r'^thanks/(\w+)/$', thanks, name='checkin-thanks'),
-    url(r'^terms/$', terms, name='checkin-terms'),
+    url(r'^customer/$', checkin.get_customer, name='checkin-get_customer'),
+    url(r'^reset/$', checkin.reset, name='checkin-reset'),
+    url(r'^status/$', checkin.status, name='checkin-status'),
+    url(r'^checkin/print/(\w+)/$', checkin.print_confirmation, name='checkin-print'),
+    url(r'^thanks/(\w+)/$', checkin.thanks, name='checkin-thanks'),
+    url(r'^terms/$', checkin.terms, name='checkin-terms'),
 ]
